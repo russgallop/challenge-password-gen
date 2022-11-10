80c4eb8dd92c82160de0d09bef36065a3e962a15
rgallop <r.russell.gallop@gmail.com>
2022-11-10 14:31:19 -0500

    added array and if statements

--- script.js
+++ script.js
@@ -1,5 +1,48 @@
 // Assignment Code
+
+  // Prompt user for password criteria
+  // 1) Password length between 8 and 128 charaters.  2) Should include: lowercase, uppercase, numbers and special charaters
+  // Validate input
+  // Gererate password based on criteria
+
+
+
 var generateBtn = document.querySelector("#generate");
+
+// set of charaters to use
+
+let upper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'S', 'Y', 'Z'];
+let lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 's', 'y', 'z'];
+let number = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let specchar = ['~', '`', '!', '@', '#', '$', '%', '%', '^', '&', '*'];
+
+  // lenLower = lower.length;
+  // lenUpper = upper.length;
+  // lenNumber = number.length;
+  // lenSpecialChar = specchar.length;
+
+  charactersToUse = [];
+  
+
+  if (useUpperCase == ture) {
+    charactersToUse.push(...upper);
+  }
+
+  if (useUpperCase == ture) {
+    charactersToUse.push(...lower);
+  }
+
+  if (useUpperCase == ture) {
+    charactersToUse.push(...number);
+  }
+
+  if (useUpperCase == ture) {
+    charactersToUse.push(...specchar);
+  }
+
+  console.log(charactersToUse);
+
+  
 
 // Write password to the #password input
 function writePassword() {
